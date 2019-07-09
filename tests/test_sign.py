db7f3dbcc32ada8856a47ca5b89812c9d0002717
Chris AtLee <chris@atlee.ca>
2019-07-09 14:30:13 -0400

    Adding tests for timestamps

--- tests/test_sign.py
+++ tests/test_sign.py
@@ -2,8 +2,8 @@ import subprocess
 from pathlib import Path
 
 import pytest
-from common import DATA_DIR, TEST_MSI_FILES, TEST_PE_FILES
-from winsign.asn1 import get_signatures_from_certificates
+from common import DATA_DIR, TEST_MSI_FILES, TEST_PE_FILES, EXPECTED_SIGNATURES
+from winsign.asn1 import get_signatures_from_certificates, id_timestampSignature
 from winsign.crypto import load_pem_cert, load_private_key, sign_signer_digest
 from winsign.pefile import is_pefile, pefile
 from winsign.sign import sign_file
@@ -42,7 +42,7 @@ def get_certificates(f):
 
 
 @pytest.mark.parametrize("test_file", TEST_PE_FILES + TEST_MSI_FILES)
-def test_osslsign_sign_file(test_file, tmp_path, signing_keys):
+def test_sign_file(test_file, tmp_path, signing_keys):
     """
     Check that we can sign with the osslsign wrapper
     """
@@ -66,7 +66,7 @@ def test_osslsign_sign_file(test_file, tmp_path, signing_keys):
             assert len(sigs[0]["certificates"]) == 1
 
 
-def test_osslsign_sign_file_dummy(tmp_path, signing_keys):
+def test_sign_file_dummy(tmp_path, signing_keys):
     """
     Check that we can sign with an additional dummy certificate for use by the
     stub installer
@@ -93,7 +93,7 @@ def test_osslsign_sign_file_dummy(tmp_path, signing_keys):
         assert len(sigs[0]["certificates"]) == 2
 
 
-def test_osslsign_sign_file_badfile(tmp_path, signing_keys):
+def test_sign_file_badfile(tmp_path, signing_keys):
     """
     Verify that we can't sign non-exe files
     """
@@ -107,3 +107,81 @@ def test_osslsign_sign_file_badfile(tmp_path, signing_keys):
         return sign_signer_digest(priv_key, digest_algo, digest)
 
     assert not sign_file(test_file, signed_file, "sha1", cert, signer)
+
+
+@pytest.mark.parametrize("test_file", EXPECTED_SIGNATURES.keys())
+def test_timestamp_old(test_file, tmp_path, signing_keys, httpserver):
+    """
+    Verify that we can sign with old style timestamps
+    """
+    signed_exe = tmp_path / "signed.exe"
+
+    priv_key = load_private_key(open(signing_keys[0], "rb").read())
+    cert = load_pem_cert(signing_keys[1].read_bytes())
+
+    def signer(digest, digest_algo):
+        return sign_signer_digest(priv_key, digest_algo, digest)
+
+    httpserver.serve_content((DATA_DIR / f"unsigned-sha1-ts-old.dat").read_bytes())
+    assert sign_file(
+        test_file,
+        signed_exe,
+        "sha1",
+        cert,
+        signer,
+        timestamp_style="old",
+        timestamp_url=httpserver.url,
+    )
+
+    # Check that we have 3 certificates in the signature
+    if is_pefile(test_file):
+        with signed_exe.open("rb") as f:
+            certificates = get_certificates(f)
+            sigs = get_signatures_from_certificates(certificates)
+            assert len(certificates) == 1
+            assert len(sigs) == 1
+            assert len(sigs[0]["certificates"]) == 3
+
+
+@pytest.mark.parametrize("test_file", EXPECTED_SIGNATURES.keys())
+def test_timestamp_rfc3161(test_file, tmp_path, signing_keys, httpserver):
+    """
+    Verify that we can sign with RFC3161 timestamps
+    """
+    signed_exe = tmp_path / "signed.exe"
+
+    priv_key = load_private_key(open(signing_keys[0], "rb").read())
+    cert = load_pem_cert(signing_keys[1].read_bytes())
+
+    def signer(digest, digest_algo):
+        return sign_signer_digest(priv_key, digest_algo, digest)
+
+    httpserver.serve_content((DATA_DIR / f"unsigned-sha1-ts-rfc3161.dat").read_bytes())
+    assert sign_file(
+        test_file,
+        signed_exe,
+        "sha1",
+        cert,
+        signer,
+        timestamp_style="rfc3161",
+        timestamp_url=httpserver.url,
+    )
+
+    # Check that we have 1 certificate in the signature,
+    # and have a counterSignature section
+    if is_pefile(test_file):
+        with signed_exe.open("rb") as f:
+            certificates = get_certificates(f)
+            sigs = get_signatures_from_certificates(certificates)
+            assert len(certificates) == 1
+            assert len(sigs) == 1
+            assert len(sigs[0]["certificates"]) == 1
+            assert any(
+                (
+                    sigs[0]["signerInfos"][0]["unauthenticatedAttributes"][i]["type"]
+                    == id_timestampSignature
+                )
+                for i in range(
+                    len(sigs[0]["signerInfos"][0]["unauthenticatedAttributes"])
+                )
+            )
